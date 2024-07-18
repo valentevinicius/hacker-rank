8557114a6b8aeef00501388ac9bf90ab8cb2e540
unknown <viniciusvramirez0108@gmail.com>
2024-07-18 15:57:55 -0300

    Updated code defaults and added readme

--- minimax.py
+++ minimax.py
@@ -1,17 +1,3 @@
-"""
-    Given five positive integers, find the minimum and maximum values
-    that can be calculated by summing exactly four of the five integers.
-    Then print the respective minimum and maximum values as a single line of two space-separated long integers.
-
-    Example:
-    arr = [1,3,5,7,9]
-
-    The minimum sum is 1+3+5+7=16
-    The maximum sum is 3+5+7+9=24
-
-    The function prints 16 24
-"""
-
 def miniMaxSum(arr):
     index = 0
     result = list()
